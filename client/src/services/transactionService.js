7479d85ae14a2b5ae85ad2a3c29a10c64a1ce963
darshan <darshanhiteshshah@gmail.com>
2025-08-29 19:31:50 +0530

    url

--- client/src/services/transactionService.js
+++ client/src/services/transactionService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a reusable Axios instance with a predefined base URL.
 const apiClient = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "process.env.REACT_APP_API_URL/api",
     headers: {
         "Content-Type": "application/json",
     },
