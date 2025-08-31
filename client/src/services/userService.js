e6ae19dd8a59a71849b90f43b854467d17968857
darshan <darshanhiteshshah@gmail.com>
2025-08-31 10:15:01 +0530

    vercel.json

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api/user`,
+    baseURL: `${import.meta.env.REACT_APP_API_URL}/api/user`,
     headers: { "Content-Type": "application/json" },
 });
 
